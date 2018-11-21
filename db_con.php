0681647acb8e0f8cc2b908dbeafedf91c9279181
edollo <samuel.maissen@hotmail.com>
2018-11-21 13:53:35 +0100

    Filenamen Angepasst

--- db_con.php
+++ db_con.php
@@ -2,7 +2,7 @@
 ####################################################
 #+------------------------------------------------+#
 #|Project:       ULWF                             |#
-#|Filename:      lib.core.php                     |#
+#|Filename:      db_con.php                       |#
 #|Licence:       © Open Licence			          |#
 #|Created by:    Anto Ivankovic / Samuel Maissen  |#
 #+------------------------------------------------+#
@@ -10,7 +10,7 @@
 
 // DB Connection
 
-$db = new mysqli("127.0.0.1", "root", "",/*"antoivan.mysql.db.hostpoint.ch", "antoivan_root", "Githubhustlers7000!",*/ "antoivan_ULoseWeFind");
+$db = new mysqli("antoivan.mysql.db.hostpoint.ch", "antoivan_root", "Githubhustlers7000!", "antoivan_ULoseWeFind");
 
 if ($db->connect_errno) {
 	printf("Connect failed: %s\n", $db->connect_error);
