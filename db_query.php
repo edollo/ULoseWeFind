4764fd3d21cc1450fdd42bac684d26f10e9f9507
edollo <samuel.maissen@hotmail.com>
2018-11-30 15:19:20 +0100

    diverses
    -Logoff Procedur verbessert (komplette session gekillt)
    -Neuer Benutzer registrieren
    -Verkleinerung und säuberung von Proceduren

--- db_query.php
+++ db_query.php
@@ -37,7 +37,15 @@ $query = "SELECT Email, Passwort FROM Person WHERE Email ='".$bindun."'";
 				}
 				else {
 					echo "invalidpw";
+					
+					//Vorgägnge damit Session komplett entfernt wird
+					session_unset();
 					session_destroy();
+					session_write_close();
+					setcookie(session_name(),'',0,'/');
+					session_regenerate_id(true);
+					session_destroy();
+					
 				}
 			}
 		}	
@@ -49,7 +57,7 @@ $query = "SELECT Email, Passwort FROM Person WHERE Email ='".$bindun."'";
 		}	
 	}
 	$result->free();	
-	$db->close();
+	
 		
 }
 
@@ -58,7 +66,7 @@ if(isset($_POST['signup_btn'])) {
     header('Refresh: 0 ; url=sign_up.html');
 }
 
-//Überprüfung Benutzername
+//Überprüfung email
 if(isset($_POST['ValidateUname'])) {
 
 	//Security /Variablenauslesen
@@ -71,16 +79,13 @@ if(isset($_POST['ValidateUname'])) {
 		//Rückgabewert 0 --> Benutzer nicht vorhanden
 		if (mysqli_num_rows($result) == 0)
 		{
-			echo "invalid";
+			echo "existsnot";
 		}
 		else
 		{
-			echo "isvalid";
+			echo "exists";
 		}
 	}	
-	
-$db->close();
-
 }
 
 //überprüfung ob passwörter übereinstimmen
@@ -100,33 +105,6 @@ if(isset($_POST['su_psw_2'])) {
 }
 
 
-//überprüfung ob email schon vorhanden
-if(isset($_POST['su_email'])) {
-
-
-	//Security /Variablenauslesen
-	$bindemail = mysqli_real_escape_string($db, $_POST['su_email']);
-		
-	//query bilden
-	$query = "SELECT Email FROM Person WHERE Email ='".$bindemail."'";
-
-	if ($result = $db->query($query)) {
-		
-		
-		//Rückgabewert 0 --> Benutzer nicht vorhanden
-		if (mysqli_num_rows($result) !== 0)
-		{
-			echo "exists";
-		}
-		else
-		{
-			echo "existsnot";
-		}
-	}
-
-	$db->close();
-}
-
 //Registrierungs Prozedur
 if(isset($_POST['su_signup_btn'])) {
 
@@ -137,19 +115,28 @@ if(isset($_POST['su_signup_btn'])) {
 	$bindpw2 = mysqli_real_escape_string($db, $_POST['su_psw_2']);
 	
 	//query's bilen
-	$query = "SELECT Email FROM Person WHERE Email ='".$bindemail."'";
-	$queryinsert = "INSERT INTO `Person`(`Name`, `Nachname`, `Email`, `Anrede_idAnrede`, `Passwort`) VALUES ('".$bindfirstname."','".$bindlastname."','".$bindemail."',1,'".$bindpw2."'";
-
+	//$query = "SELECT Email FROM Person WHERE Email ='".$bindemail."'";
+	$queryinsert = "INSERT INTO `Person`(`Name`, `Nachname`, `Email`, `Anrede_idAnrede`, `Passwort`) VALUES ('".$bindfirstname."','".$bindlastname."','".$bindemail."',1,'".$bindpw2."')";
 	
+	echo $queryinsert;
 	
-	//noch anzupassen
-	if ($result = $db->query($query)) {
-
-		echo "Registrierung Erfolgreich";
-			
+	if ($result = $db->query($queryinsert)) {
+	
+		echo $query;
+	
+		if($db->affected_rows > 0)
+		{
+			echo "success";
+		}
+		else
+		{
+			echo "fail";
+		
+		}
+	
 	}	
-
-	$db->close();
-
 }
+
+
+$db->close();
 ?>
